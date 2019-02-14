a9df10d01a7485a19bbfeee8685ea1dc6ce8fed7
qinguangming <13006397736@163.com>
2019-02-14 17:30:18 +0800

    code add

--- fileManageSystem/src/components/Directory.vue
+++ fileManageSystem/src/components/Directory.vue
@@ -1,26 +1,229 @@
 <template>
 	<div id="directory">
-		<h1>this is directory</h1>
+		<Table 
+			:columns="columns" 
+			:data="fileStatus"
+			:disabled-hover="true"
+			size='small'
+		></Table>
+		<Modal
+			v-model="showModal"
+			title="Message Box"
+			@on-ok="ok"
+			@on-cancel="cancel">
+			<p>确认删除选中文件吗？</p>
+		</Modal>
 	</div>
 </template>
 
 <script>
-import {API} from '@/api/apiService'	
+import {API} from '@/api/apiService'
+import moment from 'moment'	
 	export default {
 		data() {
 			return {
-				
+				fileStatus:[],//目录
+				filePath:'',//文件路径
+				currFilePath:'',//当前显示文件路径
+				preFilePath:'',//上一步的文件路径
+				showModal:false,//显示对话框
+				columns:[
+					{
+						title:'Name',
+						key:'pathSuffix',
+						render:(h,params)=>{
+							if(params.row.type === 'DIRECTORY'){
+								return h('div',{
+									style:{
+										cursor:'pointer'
+									},
+									on:{
+										click:()=>{
+											this.filePath = params.row.pathSuffix;
+										}
+									}
+									},[
+										h('Icon',{
+											props:{
+												type:'ios-folder'
+											}
+										}),
+										h('span',{
+											style:{
+												'margin-left':'5px'
+											}
+										},params.row.pathSuffix)
+								]);
+							}else if(params.row.type === 'FILE'){
+								return h('div',{
+									style:{
+										cursor:'pointer'
+									}
+									},[
+										h('Icon',{
+											props:{
+												type:'ios-paper'
+											}
+										}),
+										h('span',{
+											style:{
+												'margin-left':'5px'
+											}
+										},params.row.pathSuffix)
+								]);
+							}else{
+								return h('div',{
+									style:{
+										cursor:'pointer'
+									},
+									on:{
+										click:()=>{
+											//返回上一级
+											this.pre();
+										}
+									}
+								},[
+									h('Icon',{
+										props:{
+											type:'md-arrow-back'
+										}
+									}),
+									h('span',{
+										style:{
+											'margin-left':'5px'
+										}
+									},this.currFilePath)
+								]);
+							}
+						}
+					},
+					{
+						title:'Date Modify',
+						key:'modificationTime',
+						render:(h,params)=>{
+							if(params.row.modificationTime !== undefined){
+								return h('span',moment(params.row.modificationTime).format('YYYY/MM/DD'));
+							}
+						}
+					},
+					{
+						title:'File Size',
+						key:'blockSize',
+						render:(h,params)=>{
+							if(params.row.blockSize !== undefined){
+								if(params.row.blockSize>0 && params.row.blockSize<1024){
+									return h('span',`${params.row.blockSize}B`)
+								}else if(params.row.blockSize>=1024 && params.row.blockSize<1048576){
+									return h('span',`${Math.ceil(params.row.blockSize/1024)}KB`)
+								}else if(params.row.blockSize>=1048576){
+									return h('span',`${Math.ceil(params.row.blockSize/1024/1024)}MB`)
+								}else{
+									return h('span',`0B`)
+								}
+							}
+						}
+					},
+					{
+						title:'Action',
+						render:(h,params)=>{
+							if(params.row.type !== 'headBar'){
+								return h('div',{
+
+								},[
+									h('Icon',{
+										style:{
+											cursor:'pointer',
+											'font-size':'16px'
+										},
+										props:{
+											type:'md-trash'
+										},
+										on:{
+											click:()=>{
+												this.showModal = true;
+											}
+										},
+										domProps:{
+											title:'delete'
+										}
+									}),
+									h('Icon',{
+										style:{
+											'margin-left':'10px',
+											cursor:'pointer',
+											'font-size':'16px'
+										},
+										props:{
+											type:'ios-cloud-download-outline'
+										},
+										domProps:{
+											title:'download'
+										}
+									},this.currFilePath)
+								]);
+							}
+						}
+					}
+				]
 			};
 		},
+		watch:{
+			//不应该使用箭头函数来定义watcher函数,否则this 将不会按照期望指向Vue实例
+			filePath:{
+				handler:function(v,oV){
+					this.currFilePath = `${this.currFilePath}/${v}`; 
+					if(this.currFilePath === '/'){
+						this.currFilePath = '';
+					}
+				}
+			},
+			currFilePath:{
+				handler:function(v,oV){
+					this.getDirectory(v);
+				}
+			}
+		},
 		methods:{
-			getDirectory(){
-				API('get','/webhdfs/v1/?op=LISTSTATUS',{}).then(res=>{
-					console.log(res);
+			//对话框确认
+			ok(){
+				
+			},
+			//对话框取消
+			cancel(){
+				
+			},
+			//返回上一级
+			pre(){
+				var arr = this.currFilePath.split('/');
+				arr.length = arr.length - 1;
+				this.currFilePath = '';
+				this.filePath = arr.join(',').replace(/,/g,'');// /g正则 代替全部
+			},
+			//获取文件内容
+			getDirectory(pathSuffix){
+				var filePath = '';
+				this.fileStatus = [];
+				if(pathSuffix !== undefined && pathSuffix !== ''){
+					filePath = pathSuffix;
+					this.fileStatus=[
+						{
+							pathSuffix:'',
+							type:'headBar'
+						}
+					]
+				}
+				var url = `/webhdfs/v1/${filePath}?op=LISTSTATUS`;
+				API('get',url,{}).then(res=>{
+					res.FileStatuses.FileStatus.forEach((item)=>{
+						this.fileStatus.push(item);
+					});
 				});
 			}
 		},
-		created() {
-			this.getDirectory();
+		mounted() {
+			this.$nextTick(()=>{
+				this.getDirectory();
+			});
 		}
 	}
 </script>
